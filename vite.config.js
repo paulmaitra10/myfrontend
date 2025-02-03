2459ae6ff559be5794218e56506d6442347e04e6
Soudarjya <soudyg707@gmail.com>
2025-02-03 21:14:52 +0530

    proxy url changes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server:{
-    proxy:{
-      'url':'http://localhost:3000'
-    }
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
